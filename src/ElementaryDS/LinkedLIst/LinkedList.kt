d2d51e0196a2e340623311c8d90d7d74856219a8
EricRootLee <erickiokom@gmail.com>
2021-01-30 13:18:36 +0300

    SMall changes

--- src/ElementaryDS/LinkedLIst/LinkedList.kt
+++ src/ElementaryDS/LinkedLIst/LinkedList.kt
@@ -1,4 +1,19 @@
 package ElementaryDS.LinkedLIst
 
 class LinkedList<T> {
+    private var head: Node<T>? = null
+    private var tail: Node<T>? = null
+    private var size = 0
+
+    fun isEmpty(): Boolean {
+        return size == 0
+    }
+
+    override fun toString(): String {
+        return if (isEmpty()) {
+            "Empty List"
+        } else {
+            head.toString()
+        }
+    }
 }
--- src/ElementaryDS/LinkedLIst/Main.kt
+++ src/ElementaryDS/LinkedLIst/Main.kt
@@ -2,14 +2,18 @@ package ElementaryDS.LinkedLIst
 
 fun main() {
     println("Creating and linking nodes")
-    val node1 = Node(value = 1)
-    val node2 = Node(value = 2)
-    node1.next = node2
-    println(node1)
-
-
+    runNodes()
 }
 
-private infix fun String.example(function: () -> Unit) {
+fun runLinkedList() {
+    var list = LinkedList<Int>()
+}
 
+fun runNodes() {
+    val node1 = Node(value = 1)
+    val node2 = Node(value = 2)
+    val node3 = Node(value = 3)
+    node1.next = node2
+    node2.next = node3
+    println(node1)
 }
