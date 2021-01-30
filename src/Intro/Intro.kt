1fbf0e2dd1ab608024828a4b3be7dfcf2cf02f25
EricRootLee <erickiokom@gmail.com>
2021-01-30 11:59:56 +0300

    Log time

--- src/Intro/Intro.kt
+++ src/Intro/Intro.kt
@@ -1,5 +1,30 @@
 package Intro
 
-fun main(){
-    print("hello")
+fun main() {
+
+    print("Search Results :  ${pseudoBinaryContains(6750, listOf(4, 62, 3, 7, 4, 7, 9056, 56, 563, 545, 70, 45, 6750, 51, 42, 78))}")
 }
+
+//Time Complexity
+//1.logarithimic time
+fun pseudoBinaryContains(value: Int, data: List<Int>): Boolean {
+    val midNum = data.run {
+        this.sorted()
+        this.size / 2
+    }
+    if (value <= data[midNum]) {
+        for (num in 0..midNum) {
+            if (data[num] == value) {
+                return true
+            }
+        }
+    } else {
+        for (num in midNum until data.size) {
+            if (data[num] == value) {
+                return true
+            }
+        }
+    }
+    return false
+}
+
