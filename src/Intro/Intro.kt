3f79b33f330df2ca19d58fe6eac2660721132654
EricRootLee <erickiokom@gmail.com>
2021-01-30 12:12:09 +0300

    space complexity

--- src/Intro/Intro.kt
+++ src/Intro/Intro.kt
@@ -5,7 +5,7 @@ fun main() {
     print("Search Results :  ${pseudoBinaryContains(56, listOf(4, 62, 3, 7, 4, 7, 9056, 56, 563, 545, 70, 45, 6750, 51, 42, 78))}")
 }
 
-//Time Complexity
+//Time Complexity Sample
 //1.logarithimic time
 fun pseudoBinaryContains(value: Int, data: List<Int>): Boolean {
     val midNum = data.run {
@@ -27,6 +27,7 @@ fun pseudoBinaryContains(value: Int, data: List<Int>): Boolean {
     }
     return false
 }
-//2. Quasilinear time
+//Space Complexity Sample
+
 
 
