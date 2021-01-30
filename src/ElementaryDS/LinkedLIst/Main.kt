30303de3d55cfe85d023b8a45173b79b322dbda8
EricRootLee <erickiokom@gmail.com>
2021-01-30 12:50:58 +0300

    Main class

---
+++ src/ElementaryDS/LinkedLIst/Main.kt
@@ -0,0 +1,15 @@
+package ElementaryDS.LinkedLIst
+
+fun  main(){
+    println("Creating and linking nodes")
+        val  node1 = Node(value = 1)
+        val  node2 = Node(value = 2)
+        node1.next= node2
+        println(node1)
+
+
+}
+
+private infix fun String.example(function: () -> Unit) {
+
+}
