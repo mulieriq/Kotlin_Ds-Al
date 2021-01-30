61e031085b5c276835992fd0d17946d37255ce3e
EricRootLee <erickiokom@gmail.com>
2021-01-30 12:44:34 +0300

    Node data class

--- src/ElementaryDS/LinkedLIst/Node.kt
+++ src/ElementaryDS/LinkedLIst/Node.kt
@@ -1,3 +1,11 @@
 package ElementaryDS.LinkedLIst
 
-data class Node()
+data class Node<T>(var value: T, var next: Node<T>? = null) {
+    override fun toString(): String {
+        return if (next != null) {
+            "$value -> ${next.toString()}"
+        } else {
+            "$value"
+        }
+    }
+}
