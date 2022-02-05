b7395254ef9d24be290ea2db0c3499dd15351c9e
EricRootLee <erickiokom@gmail.com>
2022-02-05 23:29:52 +0300

    ..

--- src/ElementaryDS/LinkedListNew/Node.kt
+++ src/ElementaryDS/LinkedListNew/Node.kt
@@ -1,4 +1,15 @@
 package ElementaryDS.LinkedListNew
 
-class Node {
+data class Node<T>(
+    var value: T, var next: Node<T>? = null
+
+) {
+    override fun toString(): String {
+        return if (next != null) {
+            "$value -> ${next.toString()}"
+
+        } else {
+            "$value"
+        }
+    }
 }
